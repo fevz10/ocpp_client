754324b35af3dcf93f935704feb698a83f358b77
fevz10 <160207067@kocaeli.edu.tr>
2024-11-11 17:38:45 +0300

    Added source files.

---
+++ start_ocpp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while [ 1 ]
+do
+        # Define the process name
+        process_name="/root/ocpp_client_cs2"
+
+        # Check if the process is running
+        if ps aux | grep -v grep | grep "$process_name" > /dev/null; then
+                echo "Process $process_name is already running."
+                sleep 5
+        else
+                echo "Process $process_name is not running. Starting it..."
+                # Run the process
+                /root/ocpp_client_cs2 &
+                echo "Process $process_name started."
+        fi
+done
